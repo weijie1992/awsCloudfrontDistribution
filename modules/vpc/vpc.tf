0fe40412f0326447fffc8068f5ec3916231f1651
PhuaWeiJie <wei_jie_phua_from.tp@mom.gov.sg>
2024-11-17 16:50:04 +0800

    remove all import statement for vpc.tf

--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -1,8 +1,3 @@
-import {
-  to = aws_vpc.this
-  id = "vpc-003f6fd17cc777a78"
-}
-
 resource "aws_vpc" "this" {
   cidr_block       = "10.0.1.0/24"
   instance_tenancy = "default"
@@ -10,12 +5,6 @@ resource "aws_vpc" "this" {
     Name = "weijie-test"
   }
 }
-
-import {
-  to = aws_subnet.public_subnet_1a
-  id = "subnet-03b64f004651bbf53"
-}
-
 resource "aws_subnet" "public_subnet_1a" {
   vpc_id            = aws_vpc.this.id
   availability_zone = "ap-southeast-1a"
@@ -23,10 +12,6 @@ resource "aws_subnet" "public_subnet_1a" {
   tags = {
     Name = "pub-subnet-1a"
   }
-}
-import {
-  to = aws_subnet.public_subnet_1b
-  id = "subnet-0e70486df9027725b"
 }
 
 resource "aws_subnet" "public_subnet_1b" {
@@ -38,10 +23,6 @@ resource "aws_subnet" "public_subnet_1b" {
   }
 }
 
-import {
-  to = aws_internet_gateway.igw
-  id = "igw-0796f8e6aea089a15"
-}
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.this.id
   tags = {
@@ -49,10 +30,7 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
-import {
-  to = aws_route_table.rt
-  id = "rtb-0f58c2e376a084290"
-}
+
 resource "aws_route_table" "rt" {
   vpc_id = aws_vpc.this.id
   route {
@@ -61,19 +39,9 @@ resource "aws_route_table" "rt" {
   }
 }
 
-import {
-  to = aws_route_table_association.pub1a
-  id = "subnet-03b64f004651bbf53/rtb-0f58c2e376a084290"
-}
-
 resource "aws_route_table_association" "pub1a" {
   subnet_id      = aws_subnet.public_subnet_1a.id
   route_table_id = aws_route_table.rt.id
-}
-
-import {
-  to = aws_route_table_association.pub1b
-  id = "subnet-0e70486df9027725b/rtb-0f58c2e376a084290"
 }
 
 resource "aws_route_table_association" "pub1b" {
